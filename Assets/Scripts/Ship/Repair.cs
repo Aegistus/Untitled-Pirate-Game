da8dc18b532c5f0cf7309230be0dfe5242169b29
Aegistus <joe.ballanders@gmail.com>
2022-10-29 01:10:23 -0400

    Add ShipHealth Script

---
+++ Assets/Scripts/Ship/Repair.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repair
+{
+    public int bottom;
+    public int deck;
+    public int sails;
+
+    public Repair(int bottom, int deck, int sails)
+    {
+        this.bottom = bottom;
+        this.deck = deck;
+        this.sails = sails;
+    }
+}
