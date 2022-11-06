23fcbc257ad730435ad452db22076f7e55612346
mehrbawan <m.bano.awan@gmail.com>
2022-11-05 22:25:45 -0400

    Added ShipParts Script and Demo Scene

---
+++ Assets/Scripts/Ship/ShipParts.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ShipParts : MonoBehaviour
+{
+    [SerializeField] GameObject mainMast;
+    [SerializeField] GameObject foreMast;
+    [SerializeField] GameObject mizzenMast;
+    
+    [SerializeField] Transform starboardBroadsideParent;
+    [SerializeField] Transform portBroadsideParent;
+
+    [SerializeField] Transform sternCannonsParent;
+    [SerializeField] Transform prowCannonsParent;
+
+    [SerializeField] Transform ramParent;
+
+    Transform[] starboardBroadside;
+    Transform[] portBroadside;
+
+    Transform[] prowCannons;
+    Transform[] sternCannons;
+
+    void Awake()
+    {
+        starboardBroadside = starboardBroadsideParent.GetComponentsInChildren<Transform>();
+        portBroadside = portBroadsideParent.GetComponentsInChildren<Transform>();
+
+        prowCannons = prowCannonsParent.GetComponentsInChildren<Transform>();
+        sternCannons = sternCannonsParent.GetComponentsInChildren<Transform>();
+    }
+}
