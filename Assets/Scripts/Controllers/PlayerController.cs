44e43c532ab999d15c12d8ccbf90934254977ea9
Aegistus <joe.ballanders@gmail.com>
2022-11-23 14:34:59 -0500

    Add Player Aiming

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     ShipMovement movement;
     ShipWeapons weapons;
     CameraController cameraController;
+    ShipDirection aimDirection = ShipDirection.Prow;
 
     void Awake()
     {
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        aimDirection = DeterminePlayerAimDirection();
         if (Input.GetKeyDown(KeyCode.W))
         {
             movement.IncreaseSpeed();
@@ -36,10 +38,41 @@ public class PlayerController : MonoBehaviour
         float rotation = Input.GetAxis("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            weapons.FireWeaponsOnSide(ShipDirection.Starboard);
-            weapons.FireWeaponsOnSide(ShipDirection.Port);
+            weapons.FireWeaponsOnSide(aimDirection);
         }
         movement.Turn(rotation);
         cameraController.Zoom(-Input.mouseScrollDelta.y);
+    }
+
+    RaycastHit rayHit;
+    float[] directionAngles = new float[4];
+    // Finds where the player is aiming as a ShipDirection based on where the mouse is.
+    ShipDirection DeterminePlayerAimDirection()
+    {
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, 1000f);
+        ShipDirection direction = ShipDirection.Prow;
+        Vector3 mousePos = rayHit.point;
+        Vector3 mouseDir = mousePos - transform.position;
+        directionAngles[0] = Vector3.SignedAngle(transform.forward, mouseDir, Vector3.up); // prow
+        directionAngles[1] = Vector3.SignedAngle(-transform.forward, mouseDir, Vector3.up); // stern
+        directionAngles[2] = Vector3.SignedAngle(transform.right, mouseDir, Vector3.up); // starboard
+        directionAngles[3] =  Vector3.SignedAngle(-transform.right, mouseDir, Vector3.up); // port
+        // make all angles positive
+        for (int i = 0; i < directionAngles.Length; i++)
+        {
+            directionAngles[i] = Mathf.Abs(directionAngles[i]);
+        }
+        float smallest = float.MaxValue;
+        int smallestIndex = -1;
+        for (int i = 0; i < directionAngles.Length; i++)
+        {
+            if (directionAngles[i] < smallest)
+            {
+                smallest = directionAngles[i];
+                smallestIndex = i;
+            }
+        }
+        direction = (ShipDirection)smallestIndex; // this casts the index to the relevant ShipDirection
+        return direction;
     }
 }
